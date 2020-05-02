917e89650d872cf2ee1f1b3ed137ef3f47dbb83c
HalaSaadeh <halagsaadeh@gmail.com>
2020-05-02 23:17:54 +0300

    validate username upon login

--- app/src/main/java/com/example/ehprotocol/LoginActivity.java
+++ app/src/main/java/com/example/ehprotocol/LoginActivity.java
@@ -8,7 +8,14 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.client.Firebase;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,8 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private CheckBox rememberMeCB;
 
-    private static String username = "ea1ik";
-    private static String password = "123";
+
     private static boolean rememberMe;
 
     @Override
@@ -39,15 +45,36 @@ public class LoginActivity extends AppCompatActivity {
 
         loginButton.setOnClickListener(e -> {
             if(validateUsername() & validatePassword()) {
-                if (usernameText.getEditText().getText().toString().equals(username) && passwordText.getEditText().getText().toString().equals(password)) {
-                    if (rememberMeCB.isChecked())
-                        rememberMe = true;
+                Firebase userRef= new Firebase("https://ehprotocol.firebaseio.com/users/");
+                DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+                DatabaseReference userNameRef = rootRef.child("users");
+                Query queries=userNameRef.orderByChild("Username").equalTo(usernameText.getEditText().getText().toString().trim());
+                boolean[] flag = new boolean[1];
+                ValueEventListener eventListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(!dataSnapshot.exists()) {
+                            flag[0] =false;
+                            usernameText.setError("Invalid username.");
+                        }
+                        else
+                            flag[0]=true;
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {}
+                };
+                queries.addListenerForSingleValueEvent(eventListener);
+                if (flag[0]){
                     Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
-                    intent.putExtra("username", username);
+                    // intent.putExtra("username", username);
                     startActivity(intent);
                 }
+
+
+                }
             }
-        });
+        );
 
         newUser.setOnClickListener(e -> {
             Intent intent = new Intent(getApplicationContext(), CreateNewAccountActivity.class);
