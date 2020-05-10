21fc633f1d319d83d2cb2d4f34eafff43fa14f91
HalaSaadeh <halagsaadeh@gmail.com>
2020-05-10 11:21:39 +0300

    push user location when logged in

--- app/src/main/java/com/example/ehprotocol/LocationProvider.java
+++ app/src/main/java/com/example/ehprotocol/LocationProvider.java
@@ -2,8 +2,10 @@ package com.example.ehprotocol;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Looper;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -14,9 +16,18 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
+
 import com.google.android.gms.tasks.Task;
+import com.mongodb.stitch.android.core.Stitch;
+import com.mongodb.stitch.android.core.StitchAppClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateResult;
+
+import org.bson.Document;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class LocationProvider extends JobService {
     private static final String TAG = "Debug";
@@ -24,25 +35,55 @@ public class LocationProvider extends JobService {
     private static int count = 0;
     private int refreshRate = 10; // in seconds
     private FusedLocationProviderClient fusedLocationProviderClient;
-
+    private StitchAppClient stitchClient;
+    private RemoteMongoClient mongoClient;
+    private RemoteMongoCollection usersCollection;
     private static Location location;
 
     @Override
     public boolean onStartJob(JobParameters params) {
         doBackgroundWork(params);
         return true;
+
     }
 
     private void doBackgroundWork(JobParameters params) {
+        stitchClient = Stitch.getDefaultAppClient();
+        mongoClient = stitchClient.getServiceClient(RemoteMongoClient.factory, "mongodb-atlas");
+        usersCollection = mongoClient.getDatabase("COVID19ContactTracing").getCollection("Users");
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         new Thread(new Runnable() {
             @Override
             public void run() {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                String user = preferences.getString("username", null);
+                Document filterDoc = new Document().append("username", user);
+                if (user != null){
                 while (count < 15 * (60 / refreshRate)) {
                     getLocation();
                     if(location!= null) {
                         Log.d(TAG, "Latitude: " + location.getLatitude());
                         Log.d(TAG, "Longitude: " + location.getLongitude());
+
+                        Document updateDoc = new Document().append("$set",
+                                new Document().append("location", Arrays.asList(location.getLatitude(), location.getLongitude())
+                                ));
+
+                        final Task<RemoteUpdateResult> updateTask =
+                                usersCollection.updateOne(filterDoc, updateDoc);
+                        updateTask.addOnCompleteListener(new OnCompleteListener <RemoteUpdateResult> () {
+                            @Override
+                            public void onComplete(@NonNull Task <RemoteUpdateResult> task) {
+                                if (task.isSuccessful()) {
+                                    long numMatched = task.getResult().getMatchedCount();
+                                    long numModified = task.getResult().getModifiedCount();
+                                    Log.d("app", String.format("successfully matched %d and modified %d documents",
+                                            numMatched, numModified));
+                                } else {
+                                    Log.e("app", "failed to update document with: ", task.getException());
+                                }
+                            }
+                        });
                     }
                     else{
                         Log.d(TAG, "error");
@@ -60,7 +101,7 @@ public class LocationProvider extends JobService {
                 }
                 jobFinished(params, false);
                 count = 0;
-            }
+            }}
         }).start();
     }
 
