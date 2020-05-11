5acc28fde84613e398a00e6a2024b70c1c7110da
ea1ik <ea1ik@icloud.com>
2020-05-11 15:00:57 +0300

    finalized push notifications for daily cases

--- app/src/main/java/com/example/ehprotocol/DailyCasesUpdater.java
+++ app/src/main/java/com/example/ehprotocol/DailyCasesUpdater.java
@@ -6,17 +6,28 @@ import android.util.Log;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.util.Calendar;
 
 public class DailyCasesUpdater extends JobService {
     private static final String TAG = "Notifs";
     private boolean jobCancelled = false;
-
+    private final int refreshRate = 60; //60s
     int count = 0;
 
-    private String oldDate = "", newDate;
+    private static boolean unlocked = false;
+
+    private static String savedDate;
 
     private RequestQueue queue;
 
@@ -32,12 +43,59 @@ public class DailyCasesUpdater extends JobService {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while(count < 30) {
+                while(count < 15 * (60 / refreshRate)) {
+                    OkHttpClient client = new OkHttpClient();
 
+                    Request request = new Request.Builder()
+                            .url("https://covid-19-data.p.rapidapi.com/country/code?format=json&code=lb")
+                            .get()
+                            .addHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
+                            .addHeader("x-rapidapi-key", "942ffaca3amsh6182aa878ceed86p14709cjsn11a537a29ed5")
+                            .build();
 
+                    Thread thread = new Thread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            try  {
+                                Response response = client.newCall(request).execute();
+                                BufferedReader reader = new BufferedReader(new InputStreamReader(response.body().byteStream(), "UTF-8"));
+                                String line = reader.readLine();
+                                JSONTokener tokener = new JSONTokener(line);
+                                JSONArray mainArray = new JSONArray(tokener);
+                                JSONObject mainObject = mainArray.getJSONObject(0);
+                                String lastChange = mainObject.getString("lastChange");
+                                if(savedDate == null)
+                                    savedDate = lastChange;
+                                else if(!lastChange.equals(savedDate)) {
+                                    unlocked = true;
+                                    savedDate = lastChange;
+                                    int totalCases, totalActive, totalDeaths, totalRecoveries;
+
+                                    totalCases = mainObject.getInt("confirmed");
+                                    totalRecoveries = mainObject.getInt("recovered");
+                                    totalActive = totalCases - totalRecoveries;
+                                    totalDeaths = mainObject.getInt("deaths");
+
+                                    String updates = "The total number of cases for " + transform(savedDate) + " has reached " + totalCases
+                                            + ", where there are " + totalActive + " cases. The total death toll is " + totalDeaths;
+                                    NotificationsSender.sendOverNotificationChannel(getApplicationContext(), NotificationsSender.MEDIUM_PRIORITY_CHANNEL,
+                                            "Daily COVID-19 Updates", updates);
+                                }
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                    thread.start();
                     count++;
+
+                    Log.d(TAG, "Thread Finished");
+
                     try {
-                        Thread.sleep(15*1000);
+                        Thread.sleep(refreshRate*1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
--- app/src/main/java/com/example/ehprotocol/MainMenuActivity.java
+++ app/src/main/java/com/example/ehprotocol/MainMenuActivity.java
@@ -4,22 +4,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class MainMenuActivity extends AppCompatActivity {
     public static final String TAG = "FrontendDebug";
@@ -61,35 +49,7 @@ public class MainMenuActivity extends AppCompatActivity {
         });
 
         aboutButton.setOnClickListener(e->{
-            OkHttpClient client = new OkHttpClient();
 
-            Request request = new Request.Builder()
-                    .url("https://covid-19-data.p.rapidapi.com/country/code?format=json&code=lb")
-                    .get()
-                    .addHeader("x-rapidapi-host", "covid-19-data.p.rapidapi.com")
-                    .addHeader("x-rapidapi-key", "942ffaca3amsh6182aa878ceed86p14709cjsn11a537a29ed5")
-                    .build();
-
-            Thread thread = new Thread(new Runnable() {
-
-                @Override
-                public void run() {
-                    try  {
-                        Response response = client.newCall(request).execute();
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(response.body().byteStream(), "UTF-8"));
-                        String line = reader.readLine();
-                        JSONTokener tokener = new JSONTokener(line);
-                        JSONArray mainArray = new JSONArray(tokener);
-                        JSONObject mainObject = mainArray.getJSONObject(0);
-                        int confirmed = mainObject.getInt("confirmed");
-                        Log.d(TAG, ""+confirmed);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-            thread.start();
 
         });
     }
